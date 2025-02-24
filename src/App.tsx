da4e03557dc7a43ebb2520b0f35df3e9cdc7f7c5
amber-85 <amber.an.bo@gmail.com>
2025-02-24 11:48:53 +0100

    update before switch to branch statusModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-// import { useState } from 'react'
+// import { useState } from 'react'i
+import landing from './components/landing'
+import React from 'react'
 
 import './App.css'
+import HomePage from './components/landing'
 
 function App() {
-
-
   return (
-    <>
 
-    </>
   )
-}
+};
 
 export default App

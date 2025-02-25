a6bf0a9ce489bacfd4c7150970cf08f36707e9dd
amber-85 <amber.an.bo@gmail.com>
2025-02-25 13:33:28 +0100

    add onclick property to handle onClick event

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "./button.scss";
 
+// I added onClick? property here for handle the onClick event in the NoOrderStatus component for the button
+
 type ButtonProps = {
   children: React.ReactNode;
   bgColor: string;
   color: string;
+  onClick?: () => void;
 };
 
 const Button = ({
   children,
   bgColor,
   color,
+  onClick
+
 }: ButtonProps): React.JSX.Element => {
   return (
-    <button className="btn" style={{ backgroundColor: bgColor, color: color }}>
+    <button className="btn" style={{ backgroundColor: bgColor, color: color }} onClick={onClick}>
       {children}
     </button>
   );

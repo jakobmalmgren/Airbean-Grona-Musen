268565e2cbc255a5863883cf8e740a427e802ce4
amber-85 <amber.an.bo@gmail.com>
2025-02-25 13:45:26 +0100

    add scss no order status

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // import MenuList from "./components/menu/meny";
 
+
+
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import NavbarModal from "./components/navbarModal/NavbarModal";
+import NoOrderStatus from "./components/NoOrderStatus/NoOrderStatus"
+import Button from "./components/button/button";
 
 function App() {
   // usestage för o hantera true & false för menyn
